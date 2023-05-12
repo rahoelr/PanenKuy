d5d1f3b0a8ebb2a382f251c01ceb78ec71bb58e8
rahoelr <rahul.rahmatullah@mail.ugm.ac.id>
2023-05-12 21:20:18 +0700

    PanenKuy v1

---
+++ app/src/main/java/com/example/panenkuy/CaptureAct.java
@@ -0,0 +1,6 @@
+package com.example.panenkuy;
+
+import com.journeyapps.barcodescanner.CaptureActivity;
+
+public class CaptureAct extends CaptureActivity {
+}
---
+++ app/src/main/java/com/example/panenkuy/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.panenkuy;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.journeyapps.barcodescanner.ScanContract;
+import com.journeyapps.barcodescanner.ScanOptions;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button scanBtn = findViewById(R.id.btnScan);
+        scanBtn.setOnClickListener(v -> {
+            scanCode();
+        });
+
+    }
+
+    private void scanCode() {
+        ScanOptions options = new ScanOptions();
+        options.setPrompt("Volume up to flash on");
+        options.setBeepEnabled(true);
+        options.setOrientationLocked(true);
+        options.setCaptureActivity(CaptureAct.class);
+        barLauncher.launch(options);
+
+    }
+
+    ActivityResultLauncher<ScanOptions> barLauncher = registerForActivityResult(new ScanContract(), result -> {
+       if(result.getContents() != null){
+           AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+           builder.setTitle("Result");
+           builder.setMessage(result.getContents());
+           builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+               @Override
+               public void onClick(DialogInterface dialogInterface, int i) {
+                   dialogInterface.dismiss();
+               }
+           }).show();
+       }
+    });
+}
