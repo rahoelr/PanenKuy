61c9153ac37ad71db5e7513cabaaa10d0dde8c9e
rahoelr <rahul.rahmatullah@mail.ugm.ac.id>
2023-07-05 10:26:22 +0700

    New Layout PanenKuy

---
+++ app/src/main/java/com/example/panenkuy/AboutAct.java
@@ -0,0 +1,14 @@
+package com.example.panenkuy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AboutAct extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about);
+    }
+}
